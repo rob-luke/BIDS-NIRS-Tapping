ad81190df65d3b32420851db271d65618b372ead
Robert Luke <hello@robertluke.net>
2020-09-14 14:55:29 +1000

    Fix missing dates and times

--- code/raw-to-bids.py
+++ code/raw-to-bids.py
@@ -3,6 +3,8 @@ import mne
 from mne_nirs.io.snirf import write_raw_snirf
 from mne_bids import write_raw_bids, BIDSPath, print_dir_tree
 
+trial_type = {'Control': 1, 'Tapping/Left': 2,
+              'Tapping/Right': 3, 'ExperimentEnds': 15}
 
 # Loop over each participant
 for sub in range(1, 6):
@@ -20,14 +22,6 @@ for sub in range(1, 6):
     # Create BIDS path and write to file
     snirf_path = dname + "/" + bpath.basename + ".snirf"
     write_raw_snirf(raw, snirf_path)
-
-
-trial_type = {'Control': 1, 'Tapping/Left': 2,
-              'Tapping/Right': 3, 'ExperimentEnds': 15}
-
-# Loop over each participant
-for sub in range(1, 6):
-    subject_id = "%02d" % sub
 
     # Read source data
     raw = mne.io.read_raw_snirf(snirf_path, preload=False)
