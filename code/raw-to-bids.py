7f07a0dc4f736f3135eeee98a08a449389c6508c
Robert Luke <hello@robertluke.net>
2021-06-17 09:23:21 +1000

    Add duration

--- code/raw-to-bids.py
+++ code/raw-to-bids.py
@@ -1,10 +1,12 @@
 import os
+import numpy as np
 import mne
 from mne_nirs.io.snirf import write_raw_snirf
 from mne_bids import write_raw_bids, BIDSPath, print_dir_tree
 
 trial_type = {'Control': 1, 'Tapping/Left': 2,
               'Tapping/Right': 3, 'ExperimentEnds': 15}
+stimulus_duration = 5
 
 # Loop over each participant
 for sub in range(1, 6):
@@ -25,6 +27,8 @@ for sub in range(1, 6):
 
     # Read source data
     raw = mne.io.read_raw_snirf(snirf_path, preload=False)
+    raw.annotations.duration = np.ones(raw.annotations.duration.shape) * \
+                               stimulus_duration
     raw.info['line_freq'] = 50  # specify line frequency as required by BIDS
     events, event_id = mne.events_from_annotations(raw,
                                                    {'1.0': 1, '15.0': 15,
@@ -32,5 +36,4 @@ for sub in range(1, 6):
 
     bids_path = BIDSPath(subject=subject_id, task='tapping',
                          datatype='nirs', root='../')
-    write_raw_bids(raw, bids_path, events_data=events,
-                   event_id=trial_type, overwrite=True)
+    write_raw_bids(raw, bids_path, overwrite=True)
